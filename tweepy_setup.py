eb16afc518b656b3c5effc04a9bc1c770b4fa158
MushroomDevs-crypto <mushroomdevs@hotmail.com>
2025-01-07 23:10:58 -0300

    teste2

--- tweepy_setup.py
+++ tweepy_setup.py
@@ -14,8 +14,17 @@ auth = tweepy.OAuth1UserHandler(
 
 
 
-client = tweepy.Client(bearer_token=bearer_token)
+client = tweepy.Client(
+    bearer_token=bearer_token,
+    consumer_key=api_key,
+    consumer_secret=api_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret
+)
 
+auth = tweepy.OAuth1UserHandler(
+    api_key, api_secret, access_token, access_token_secret
+)
 api = tweepy.API(auth)
 
 client.create_tweet(text="Hello World")
